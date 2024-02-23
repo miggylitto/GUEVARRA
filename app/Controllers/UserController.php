564c035d2bc5f2bfae69adf20234fe32b69c0219
miggylitto <guevarracarlo88@gmail.com>
2024-02-23 16:09:03 +0800

    first commit

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+    
+    public function addUser() {
+        return view('user/add');
+    }
+}
+
---
+++ app/Views/user/add.php
@@ -0,0 +1,5 @@
+<?=$this->extend('layout/main')?>
+
+<?=$this->section('content')?>
+<h1>This is user add page.</h1>
+<?=$this->endSection('content')?>
